6d723bd18dc76f8380da8aaf3a4b3f69f46abd44
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2023-11-18 13:02:56 +0100

    eliminar fitxers

--- autoencoder/utils.py
+++ autoencoder/utils.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
 # Function to plot loss
-def plot_losses(train_losses, test_losses, path):
+def plot_losses(train_losses, test_losses, path, name, name1, name2):
     plt.figure(figsize=(10, 5))
-    plt.plot(train_losses, label='Train Loss')
-    plt.plot(test_losses, label='Test Loss')
+    plt.plot(train_losses[1:], label=name1)
+    plt.plot(test_losses[1:], label=name2)
     plt.xlabel('Batch')
     plt.ylabel('Loss')
     plt.legend()
     plt.title('Autoencoder Training and Testing Loss')
-    plt.savefig(f"{path}loss.png")
+    plt.savefig(f"{path}{name}.png")
