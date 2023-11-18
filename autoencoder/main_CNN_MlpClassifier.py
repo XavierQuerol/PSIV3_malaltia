4ad53e3d947cc79d3644e0a9fe07f0803677294e
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2023-11-18 12:52:10 +0100

    canvis

--- autoencoder/main_CNN_MlpClassifier.py
+++ autoencoder/main_CNN_MlpClassifier.py
@@ -8,14 +8,17 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.io import read_image
+import random
 
 from model_autoencoder import Autoencoder
 from model_CNN_MlpClassifier import Classifier
+from torchvision import models
 from dataset import ImagesDataset
 from utils import plot_losses
-from torchvision import models
 
 from config import *
+
+model_to_train = "resnet"
 
 metadata = pd.read_csv(METADATA_FILE)
 window_metadata = pd.read_csv(WINDOW_METADATA_FILE)
@@ -24,12 +27,16 @@ directories = [dir.path for dir in os.scandir(ANNOTATED_PATCHES_DIR) if dir.is_d
 
 # Define transformations and data loaders for training and testing
 train_transform = transforms.Compose([
-    transforms.Resize((128, 128), antialias=True),
-    transforms.Normalize(mean=[0.5], std=[0.5])  # Adjust mean and std as needed
+    transforms.Resize((64, 64), antialias=True),
+    transforms.RandomHorizontalFlip(p=0.5),  
+    transforms.RandomVerticalFlip(p=0.5),  
+    transforms.RandomPerspective(distortion_scale=0.5, p=0.2),  # Adjust distortion_scale as needed
+    transforms.RandomRotation(degrees=(0, 360)),  # Rotate in all angles
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Adjust mean and std as needed
 ])
 
 test_transform = transforms.Compose([
-    transforms.Resize((128, 128), antialias=True),
+    transforms.Resize((64, 64), antialias=True),
     transforms.Normalize(mean=[0.5], std=[0.5])  # Adjust mean and std as needed
 ])
 
@@ -52,7 +59,13 @@ for d in os.listdir(ANNOTATED_PATCHES_DIR):
             except:
                 continue
 
+combined = list(zip(files, targets))
 
+# Shuffle the combined list
+random.shuffle(combined)
+
+# Extract the shuffled lists
+files, targets = zip(*combined)
 data = files[:int(len(files)*0.8)]
 targets_train = targets[:int(len(files)*0.8)]
 train_dataset = ImagesDataset(data=data, targets=targets_train, data_dir=ANNOTATED_PATCHES_DIR, transform=train_transform)
@@ -61,32 +74,52 @@ train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 data = files[int(len(files)*0.8):]
 targets_test = targets[int(len(files)*0.8):]
 test_dataset = ImagesDataset(data=data, targets=targets_test, data_dir=ANNOTATED_PATCHES_DIR, transform=test_transform)
-test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)  # No need to shuffle for testing
+test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)  # No need to shuffle for testing
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = "cpu"
 
-model = models.resnet50(pretrained=True).to(device)
-for param in model.parameters():
-    param.requires_grad = False
+def initialize_weights(m):
+        if isinstance(m, nn.Conv2d):
+            nn.init.xavier_normal_(m.weight.data)
+            nn.init.constant_(m.bias.data, 0)
+        elif isinstance(m, nn.Linear):
+            nn.init.xavier_normal_(m.weight.data)
 
-# Modify the last layer for your specific classification task
-num_ftrs = model.fc.in_features
-model.fc = nn.Linear(num_ftrs, 2)
+if model_to_train == "our_classifier":
+    model = Classifier().to(device)
+    model.apply(initialize_weights)
 
-ones = sum(targets)/len(targets)
+elif model_to_train == "resnet":
+    model = models.resnet50(pretrained=True).to(device)
+    for param in model.parameters():
+        param.requires_grad = False
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Sequential(
+    nn.Linear(num_ftrs, 128),  # Replace the last fully connected layer with your custom layers
+    nn.ReLU(),
+    nn.Dropout(0.5),
+    nn.Linear(128, 2)  # Assuming your final output has 2 classes, modify this as needed
+    )
+    model.fc.apply(initialize_weights)
+
+    for param in model.layer4.parameters():
+        param.requires_grad = True
+
+ones = sum(targets[:int(len(files)*0.8)])/len(targets[:int(len(files)*0.8)])
 zeros = 1 - ones
-pos_weight = ones / (1 - ones)
 
-criterion = torch.nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([1,ones/zeros*2]))
+criterion = torch.nn.CrossEntropyLoss(weight=torch.Tensor([1, zeros / ones]))
 #criterion = nn.BCELoss(weight=torch.Tensor(1000))
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
 train_losses = []
 test_losses = []
+train_accuracies = []
+test_accuracies = []
 
 plot_interval = 10  # Define the interval for plotting losses
-num_epochs = 5
+num_epochs = 100
 
 for epoch in range(num_epochs):
     # Training loop
@@ -99,13 +132,12 @@ for epoch in range(num_epochs):
         labels = labels.to(device)
         optimizer.zero_grad()
         outputs = model(images).reshape([len(images), -1])
-        outputs = torch.sigmoid(outputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
 
-        predicted = torch.argmax(outputs, dim=1)
+        predicted = torch.argmax(torch.sigmoid(outputs), dim=1)
         labels = torch.argmax(labels, dim=1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
@@ -127,30 +159,33 @@ for epoch in range(num_epochs):
             images = images.to(device)
             labels = labels.to(device)
             test_outputs = model(images).reshape([len(images), -1])
-            test_outputs = torch.sigmoid(test_outputs)
             test_loss = criterion(test_outputs, labels)
             total_loss_test += test_loss.item()
 
             # Calculate accuracy
 
-            predicted = torch.argmax(test_outputs, dim=1)
+            predicted = torch.argmax(torch.sigmoid(test_outputs), dim=1)
             labels = torch.argmax(labels, dim=1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
     # Calculate average test loss and accuracy
     average_loss_test = total_loss_test / len(test_dataloader)
-    accuracy = 100 * correct / total
+    accuracy_test = 100 * correct / total
 
-    print(f'Epoch [{epoch + 1}/{num_epochs}], Test Loss: {average_loss_test:.4f}, Accuracy: {accuracy:.2f}%')
+    print(f'Epoch [{epoch + 1}/{num_epochs}], Test Loss: {average_loss_test:.4f}, Accuracy: {accuracy_test:.2f}%')
 
     # Append losses for plotting
     train_losses.append(average_loss)
     test_losses.append(average_loss_test)
+    train_accuracies.append(accuracy)
+    test_accuracies.append(accuracy_test)
 
     # Plot final loss
-    if (epoch + 1) % plot_interval == 0:
-        plot_losses(train_losses, test_losses, PLOT_LOSS_DIR)
+    if epoch != 0:
+        plot_losses(train_losses, test_losses, PLOT_LOSS_DIR, f"main_{model_to_train}_losses", "Train loss", "Test loss")
+        plot_losses(train_accuracies, test_accuracies, PLOT_LOSS_DIR, f"main_{model_to_train}_accuracy", "Train accuracy", "Test accuracy")
+
 
 # Save the trained model if needed
 torch.save(model.state_dict(), f'{SAVE_MODEL_DIR}model_MLP.pth')
--- autoencoder/model_CNN_MlpClassifier.py
+++ autoencoder/model_CNN_MlpClassifier.py
@@ -5,24 +5,22 @@ class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=4, stride=4, padding=1),  # Convolutional layer 1
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=4),  # Max pooling after Convolutional layer 1
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # Convolutional layer 2
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=4),  # Max pooling after Convolutional layer 2
-            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),  # Convolutional layer 3
-            nn.ReLU(),
+            nn.Conv2d(3, 8, kernel_size=3, stride=2, padding=1),  # Convolutional layer 1
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Max pooling after Convolutional layer 1
+            nn.Conv2d(8, 16, kernel_size=3, stride=2, padding=1),  # Convolutional layer 2
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Max pooling after Convolutional layer 1
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),  # Convolutional layer 2
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Max pooling after Convolutional layer 1
+            nn.Flatten(),
+
         )
 
         self.predict = nn.Sequential(
-            nn.Linear(256, 16),
+            nn.Linear(32, 16),
             nn.Linear(16, 2)
         )
 
     def forward(self, x):
         x = self.encoder(x)
-        x = torch.flatten(x, start_dim=1)
         x = self.predict(x)
-        x = torch.sigmoid(x)
         return x
